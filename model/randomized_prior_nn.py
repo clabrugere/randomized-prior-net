be1b6bdcae78f3d1f1f565a687a4b951b93401db
clement.labrugere <clement.labrugere@teads.com>
2023-08-30 09:22:57 +0200

    add license

--- model/randomized_prior_nn.py
+++ model/randomized_prior_nn.py
@@ -7,7 +7,6 @@ class MLP(tf.keras.Model):
     def __init__(self, num_hidden, dim_hidden, dim_out=None, dropout=0.0, batch_norm=True, name="MLP"):
         super().__init__(name=name)
 
-        self.dim_out = dim_out
         self.blocks = tf.keras.Sequential(name="MLP")
         for _ in range(num_hidden - 1):
             self.blocks.add(tf.keras.layers.Dense(dim_hidden))
